a49c6023effbd7e0e000f937e8dfe5ea468cfa2a
K0lles <oleksijkolotilo63@gmail.com>
2023-02-26 21:35:04 +0200

    created models and downloaded main libraries. added requirements.txt

---
+++ flats/models.py
@@ -0,0 +1,263 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+from users.models import User
+
+
+class ResidentialComplex(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    map_code = models.TextField()
+    description = models.TextField()
+    photo = models.ImageField(upload_to='residential_complex/photos/')
+
+    class Status(models.TextChoices):
+        # TODO: which statuses could have complex?
+        flat = ('flats', 'Квартири')
+        cottage = ('cottage', 'Котедж')
+        many_floors = ('many-floors', 'Багатоповерхівка')
+        secondary_market = ('secondary-market', 'Вторинний ринок')
+
+    status = models.CharField(max_length=50, choices=Status.choices)
+    price_for_meter = models.FloatField(validators=[MinValueValidator(1.00)])
+    min_price = models.FloatField(validators=[MinValueValidator(1.00)])
+
+    class HouseType(models.TextChoices):
+        cottage = ('cottage', 'Котедж')
+        many_floors = ('many-floors', 'Багатоповерхівка')
+        secondary_market = ('secondary-market', 'Вторинний ринок')
+
+    house_type = models.CharField(max_length=50, choices=HouseType.choices)
+
+    class HouseClass(models.TextChoices):
+        lux = ('lux', 'Люкс')
+        elite = ('elite', 'Елітний')
+        common = ('common', 'Загальний')
+
+    house_class = models.CharField(max_length=50, choices=HouseClass.choices)
+
+    class BuildingTechnology(models.TextChoices):
+        bricks = ('bricks', 'Цегляний')
+        # TODO: are there many other types?
+
+    building_technology = models.CharField(max_length=50, choices=BuildingTechnology.choices)
+
+    class TerritoryType(models.TextChoices):
+        closed = ('closed', 'Закрита')
+        closed_and_secured = ('closed-and-secured', 'Закрита та охороняється')
+        opened = ('opened', 'Відкрита')
+
+    territory_type = models.CharField(max_length=50, choices=TerritoryType.choices)
+    sea_distance = models.IntegerField(validators=[MinValueValidator(1)], blank=True, null=True)
+    ceiling_altitude = models.IntegerField(validators=[MinValueValidator(0)])
+    gas = models.BooleanField(default=True)
+
+    class HeatingChoice(models.TextChoices):
+        central = ('centralized', 'Централізоване')
+        # TODO: are there other types of heating?
+
+    heating = models.CharField(max_length=50, choices=HeatingChoice.choices)
+    electricity = models.BooleanField(default=True)
+
+    class SewageChoice(models.TextChoices):
+        central = ('centralized', 'Централізоване')
+        # TODO: are there other types of sewage?
+
+    sewage = models.CharField(max_length=50, choices=SewageChoice.choices)
+
+    class WaterSupplyChoice(models.TextChoices):
+        central = ('centralized', 'Централізоване')
+        # TODO: are there other types of water supply?
+
+    water_supply = models.CharField(max_length=50, choices=WaterSupplyChoice.choices)
+
+    class ArrangementChoice(models.TextChoices):
+        justice = ('justice', 'Юстиція')
+        # TODO: are there other types of arrangements?
+
+    arrangement = models.CharField(max_length=50, choices=ArrangementChoice.choices)
+
+    class PaymentChoice(models.TextChoices):
+        mortgage = ('mortgage', 'Іпотека')
+        parent_capital = ('parent-capital', 'Материнський капітал')
+
+    payment = models.CharField(max_length=50, choices=PaymentChoice.choices)
+
+    class PurposeChoice(models.TextChoices):
+        living_building = ('living-building', 'Житловий будинок')
+        # TODO: the same
+
+    purpose = models.CharField(max_length=50, choices=PurposeChoice.choices)
+
+    class ContractSumChoice(models.TextChoices):
+        full = ('full', 'Повна')
+        part = ('part', 'Частинами')
+
+    sum_in_contract = models.CharField(max_length=20, choices=ContractSumChoice.choices)
+
+
+class Document(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    document = models.FileField(upload_to='residential_complex/documents/')
+
+
+class News(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Addition(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.ImageField(upload_to='additions/logos/')
+
+
+class AdditionInComplex(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.CASCADE)
+    addition = models.ForeignKey(Addition, on_delete=models.SET_NULL, blank=True, null=True)
+    turned_on = models.BooleanField(default=False)
+
+
+class Section(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+
+class Floor(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+
+class Corps(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+
+class Gallery(models.Model):
+    pass
+
+
+class Photo(models.Model):
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photos/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    sequence_number = models.IntegerField(validators=[MinValueValidator(0)])
+
+
+class Flat(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.PROTECT)
+    floor = models.ForeignKey(Floor, on_delete=models.PROTECT)
+    section = models.ForeignKey(Section, on_delete=models.PROTECT)
+    corps = models.ForeignKey(Corps, on_delete=models.PROTECT)
+    gallery = models.OneToOneField(Gallery, on_delete=models.PROTECT)
+    district = models.CharField(max_length=200)
+    micro_district = models.CharField(max_length=200)
+    room_amount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(6)])
+    scheme = models.ImageField(upload_to='flats/schemes/')
+    square = models.IntegerField(validators=[MinValueValidator(1)])
+    price = models.IntegerField(validators=[MinValueValidator(1)])
+
+    class ConditionType(models.TextChoices):
+        draft = ('draft', 'Чорновик')
+        living_condition = ('living-condition', 'Житлова')
+
+    condition = models.CharField(max_length=20, choices=ConditionType.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ChessBoard(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.PROTECT)
+    corps = models.ForeignKey(Corps, on_delete=models.PROTECT)
+    number = models.CharField(max_length=8)
+
+    class Type(models.TextChoices):
+        draft = ('draft', 'Чорновик')
+        # TODO: the same
+
+    chess_type = models.CharField(max_length=20, choices=Type.choices)
+    created_at = models.DateField(auto_now_add=True)
+
+
+class ChessBoardFlat(models.Model):
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.PROTECT)
+    flat = models.ForeignKey(Flat, on_delete=models.PROTECT)
+    chessboard = models.ForeignKey(ChessBoard, on_delete=models.PROTECT)
+    gallery = models.OneToOneField(Gallery, on_delete=models.PROTECT)
+    accepted = models.BooleanField(default=False)
+    address = models.TextField()
+
+    class FoundationDocumentType(models.TextChoices):
+        property = ('property', 'Власність')
+
+    class PurposeChoice(models.TextChoices):
+        apartments = ('apartments', 'Квартири')
+        # TODO: are these purposes are equal to purposes in ResidentialComplex model?
+
+    purpose = models.CharField(max_length=20, choices=PurposeChoice.choices)
+    room_amount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class PlanningChoice(models.TextChoices):
+        studio_bathroom = ('studio-bathroom', 'Студія і ванна')
+        studio = ('studio', 'Студія')
+
+    planning = models.CharField(max_length=30, choices=PlanningChoice.choices)
+
+    class HouseCondition(models.TextChoices):
+        repair_required = ('repair-required', 'Потрібен ремонт')
+        good = ('good', 'Задовільний')
+        # TODO: are these house conditions the same as in ResidentialComplex model?
+
+    house_condition = models.CharField(max_length=30, choices=HouseCondition.choices)
+    overall_square = models.FloatField(validators=[MinValueValidator(1)])
+    kitchen_square = models.FloatField(validators=[MinValueValidator(1)])
+    has_balcony = models.BooleanField(default=True)
+
+    class HeatingType(models.TextChoices):
+        gas = ('gas', 'Газ')
+        centralized = ('centralized', 'Централізоване')
+        # TODO: the same
+
+    heating_type = models.CharField(max_length=20, choices=HeatingType.choices)
+
+    class PaymentOption(models.TextChoices):
+        parent_capital = ('parent-capital', 'Материнський капітал')
+        # TODO: the same
+
+    payment_option = models.CharField(max_length=20, choices=PaymentOption.choices)
+    agent_commission = models.IntegerField(validators=[MinValueValidator(0)])
+
+    class CommunicationMethod(models.TextChoices):
+        phone_messages = ('phone-messages', 'Телефон і повідомлення')
+        phone = ('phone', 'Тільки телефон')
+        messages = ('messages', 'Тільки повідомлення')
+
+    communication_method = models.CharField(max_length=40, choices=CommunicationMethod.choices)
+    description = models.TextField()
+    price = models.IntegerField(validators=[MinValueValidator(1)])
+    main_photo = models.ImageField(upload_to='chessboard/main_photos/')
+
+
+class PromotionType(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(validators=[MinValueValidator(0.00)])
+    efficiency = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+
+
+class Promotion(models.Model):
+    chessboard_flat = models.ForeignKey(ChessBoardFlat, on_delete=models.CASCADE)
+    promotion_type = models.ForeignKey(PromotionType, on_delete=models.PROTECT)
+    logo = models.ImageField(upload_to='promotions/')
+    header = models.CharField(max_length=200, null=True)
+
+    class ColorChoice(models.TextChoices):
+        green = ('green', 'Зелений')
+        red = ('red', 'Червоний')
+
+    color = models.CharField(max_length=15, choices=ColorChoice.choices, blank=True, null=True)
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chessboard_flat = models.ForeignKey(ChessBoardFlat, on_delete=models.CASCADE)
+    residential_complex = models.ForeignKey(ResidentialComplex, on_delete=models.CASCADE)
---
+++ users/models.py
@@ -0,0 +1,133 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Role(models.Model):
+
+    class RoleChoice(models.TextChoices):
+        user = ('user', 'Користувач')
+        manager = ('manager', 'Менеджер')
+        administrator = ('admin', 'Адміністратор')
+        builder = ('builder', 'Забудовник')
+
+    role = models.CharField(max_length=55, choices=RoleChoice.choices)
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        admin_role = Role.objects.get(role='admin')
+        extra_fields.setdefault('role', admin_role)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    role = models.ForeignKey(Role, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    phone = PhoneNumberField(blank=True, null=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=500)
+    is_active = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(default=False)
+
+    class NotificationChoices(models.TextChoices):
+        me = 'me'
+        me_agent = 'me-agent'
+        agent = 'agent'
+        disabled = 'disabled'
+
+    notifications = models.CharField(max_length=15, choices=NotificationChoices.choices, default='me')
+    turn_calls_to_agent = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password', 'name', 'surname']
+
+    def __str__(self):
+        return self.email
+
+    objects = UserManager()
+
+
+class Notary(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    phone = PhoneNumberField()
+
+
+class Subscription(models.Model):
+
+    class TypeChoice(models.TextChoices):
+        common = 'common'
+        lux = 'lux'
+
+    type = models.CharField(max_length=25, choices=TypeChoice.choices)
+    sum = models.FloatField(validators=[MinValueValidator(0.00, _('Sum cannot be less than 0.00'))])
+
+
+class UserSubscription(models.Model):
+    subscription = models.ForeignKey(Subscription, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    expire_date = models.DateTimeField()
+    auto_pay = models.BooleanField(default=False)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='user', blank=True, null=True)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='manager', blank=True, null=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class SavedFilter(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class HouseType(models.TextChoices):
+        all = ('all', 'Всі')
+        secondary_market = ('secondary-market', 'Вторинний ринок')
+        many_floors = ('many-floors', 'Багатоповерхівка')
+        cottage = ('cottage', 'Котедж')
+
+    house_type = models.CharField(max_length=40, choices=HouseType.choices)
+
+    class HouseStatus(models.TextChoices):
+        rented = ('rented', 'Здана')
+        # TODO: ask about other statuses
+
+    house_status = models.CharField(max_length=30, choices=HouseStatus.choices)
+    district = models.CharField(max_length=200)
+    micro_district = models.CharField(max_length=200)
+    room_amount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], blank=True, null=True)
+    price_from = models.IntegerField(validators=[MinValueValidator(0)])
+    price_to = models.IntegerField(validators=[MinValueValidator(0)])
+    square_from = models.IntegerField(validators=[MinValueValidator(0)])
+    square_to = models.IntegerField(validators=[MinValueValidator(0)])
+    purpose = models.CharField(max_length=200, blank=True, null=True)
+
+    class PaymentOption(models.TextChoices):
+        mortgage = ('mortgage', 'Іпотека')
+        parent_capital = ('parent-capital', 'Материнський капітал')
+
+    payment_option = models.CharField(max_length=40, choices=PaymentOption.choices)
+
+    class HouseCondition(models.TextChoices):
+        repair_required = ('repair-required', 'Потребує ремонту')
+        good = ('good', 'Задовільний')
+
+    housing_condition = models.CharField(max_length=30, choices=HouseCondition.choices)
